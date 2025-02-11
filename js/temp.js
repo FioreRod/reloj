90318eab1c6ed2a153ef2bea7dc0ee22c79d3cac
Rodríguez Fiorella <fio.rodriiguez.01@gmail.com>
2025-02-10 22:10:40 -0300

    Temporizador funcional.

--- js/temp.js
+++ js/temp.js
@@ -1,11 +1,56 @@
-const mins = document.querySelector("#mins")
-const buttons = document.querySelectorAll("button");
+let inputs, clock, alarm, hours, minutes, seconds, repeater; 
 
-const timer = setInterval (()=> {
-    console.log(mins);
-    mins--;
-    if (mins==0){
-        clearInterval(timer)
+window.addEventListener('load', () => { 
+    inputs = Array.from(document.getElementsByClassName('number')); 
+    clock = document.querySelector('#temporizador'); 
+    alarm = new Audio('--'); 
+});
+
+
+function startTimer() {
+    parseTime();
+    setTimer();  
+    countdown()  
+}
+
+function setTimer() {
+    clock.innerHTML = `<p class="number">${hours > 9 ? hours : ('0' + hours)}</p><span>hs</span>
+                        <p class="number">${minutes > 9 ? minutes : ('0' + minutes)}</p><span>min</span>
+                        <p class="number">${seconds > 9 ? seconds : ('0' + seconds)}</p><span>sec</span>`;
+}
+
+function parseTime() {
+    hours = Number(inputs[0].value);
+    minutes = Number(inputs[1].value);
+    seconds = Number(inputs[2].value);
+}
+
+function countdown() {
+    repeater = setInterval(runner, 1000);
+}
+
+function runner() {
+    if (seconds > 0) {
+        seconds--;
+    } else {
+        if (minutes > 0) {
+            seconds = 59;
+            minutes--;
+        } else {
+            if (hours > 0) {
+                seconds = 59;
+                minutes = 59;
+                hours--;
+            } else {
+                alarm.play();
+            }
+        }
     }
-})
 
+    setTimer();
+}
+
+function stopTimer() {
+    clearInterval(repeater);
+    location.reload();
+}
